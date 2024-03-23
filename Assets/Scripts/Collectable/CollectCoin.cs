bd829abb5b241e1d4dcf4677c234d5981a647b29
Arun Kushwaha <121769639+Arun-kushwaha007@users.noreply.github.com>
2024-03-23 16:38:47 +0530

    game

---
+++ Assets/Scripts/Collectable/CollectCoin.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectCoin : MonoBehaviour
+{
+    public AudioSource coinFX;
+
+    void OnTriggerEnter(Collider other)
+    {
+        coinFX.Play();
+        CollactableControl.coinCount += 1;
+        this.gameObject.SetActive(false);
+    }
+}
+
+
